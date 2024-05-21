ba90c7ccd6211405f68139a676efe61493035ab3
sabilafiridas <sabilafirdias@gmail.com>
2024-05-21 13:38:40 +0700

    membuat header jantung

--- komposisi/jantung.h
+++ komposisi/jantung.h
@@ -1,13 +1,3 @@
 #ifndef JANTUNG_H
 #define JANTUNG_H
 
-class jantung {
-public:
-	jantung() {
-		cout << "jantung dihidupkan\n";
-	}
-	~jantung() {
-		cout << "jantung dimatikan\n";
-	}
-};
-#endif
