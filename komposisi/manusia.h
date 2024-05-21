d98f0eadd7b34b60283ce7508f8af78447a73950
sabilafiridas <sabilafirdias@gmail.com>
2024-05-21 13:40:28 +0700

    membuat header manusia

--- komposisi/manusia.h
+++ komposisi/manusia.h
@@ -2,17 +2,3 @@
 #define MANUSIA_H
 using namespace std;
 
-class manusia {
-public:
-	string name;
-	jantung varJantung;		//cara relasinya
-
-	manusia(string pName)
-		: name(pName) {
-		cout << name << " hidup\n";
-	}
-	~manusia() {
-		cout << name << " mati\n";
-	}
-};
-#endif
