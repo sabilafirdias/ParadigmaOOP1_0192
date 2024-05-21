c9297d7298da2f60cf6898ed038352ee3dc4e229
sabilafiridas <sabilafirdias@gmail.com>
2024-05-21 13:52:27 +0700

    membuat void cetakAnak

--- agregasi/ibu.h
+++ agregasi/ibu.h
@@ -20,4 +20,9 @@ void ibu::tambahAnak(anak* pAnak) {
 	daftar_anak.push_back(pAnak);		//push_back: untuk memasukkan data
 }
 
-
+void ibu::cetakAnak() {
+	cout << "Daftar Anak dari Ibu \"" << this->nama << "\':\n";
+	for (auto& a : daftar_anak) {		//auto&: akan otomatis membaca tipe datanya serta panjang datanya
+		cout << a->nama << "\n";
+	}
+	
