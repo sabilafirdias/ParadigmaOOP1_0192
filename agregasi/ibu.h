5165685dea92f0db11f7ebaa00b1ab662804584b
sabilafiridas <sabilafirdias@gmail.com>
2024-05-21 13:51:21 +0700

    membuat construction dan deconstruction

--- agregasi/ibu.h
+++ agregasi/ibu.h
@@ -6,4 +6,10 @@ public:
 	string nama;
 	vector<anak*> daftar_anak;		//vector = array. sebagai wadah untuk menampung banyak anak
 
+	ibu(string pNama) :nama(pNama) {
+		cout << "Ibu \"" << nama << "\" ada\n";
+	}
+	~ibu() {
+		cout << "Ibu \"" << nama << "\" tidak ada\n";
+	}
 	
