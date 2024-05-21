ce5a7fcd8489412f476c91647c5022dbd9607c40
sabilafiridas <sabilafirdias@gmail.com>
2024-05-21 14:30:25 +0700

    membuat construction dan deconstruction orang

--- inheritance/inheritance.cpp
+++ inheritance/inheritance.cpp
@@ -5,6 +5,14 @@ using namespace std;
 class orang {
 public:
     string nama;
+
+    orang(string pNama) :
+        nama(pNama) {
+        cout << "orang dibuat\n" << endl;
+    }
+    ~orang() {
+        cout << "orang dihapus\n" << endl;
+    }
 };
 
 int main()
